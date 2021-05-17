e9b31808ad74683c28438dfbd509603dc3eeca6d
Kim Liu <kimliu@stanford.edu>
2021-05-16 23:40:37 -0700

    added some comments, deleted old funs in process

--- process_raw_data.py
+++ process_raw_data.py
@@ -1,6 +1,13 @@
 import numpy as np 
 
 def rans_filter(x, w):
+
+    '''
+    Moving average window
+    Altered at endpoint for symmetry condition in centerline
+        Ok at beginning because of no slip
+        Currently only works for w = 3
+    '''
     if x.ndim > 1 and x.shape[1] > 1:
         out = np.empty_like(x)
         for ii in range(x.shape[1]):
@@ -59,22 +66,10 @@ def compute_invariants(shat,rhat):
         normalization - k/2/eps
 
     '''
-
-
-    #lam = np.empty([1,7])
-    #lam[0,0] = np.trace(shat)
-    #lam[0,1] = np.trace(shat @ shat)
-    #lam[0,2] = np.trace(shat @ shat @ shat)
-    #lam[0,3] = np.trace(rhat @ rhat)
-    #lam[0,4] = np.trace(rhat @ rhat @ shat)
-    #lam[0,5] = np.trace(rhat @ rhat @ shat @ shat)
-    #lam[0,6] = np.trace(rhat @ rhat @ shat @ rhat @ shat @ shat)
-
+ 
     lam = np.empty([1,3])
     lam[0,0] = np.trace(shat @ shat)
     lam[0,1] = np.trace(rhat @ rhat)
-    #lam[0,2] = np.trace(shat @ shat @ shat)
-    #lam[0,3] = np.trace(rhat @ rhat @ shat)
     lam[0,2] = np.trace(rhat @ rhat @ shat @ shat)
 
     return lam 
@@ -87,13 +82,6 @@ def compute_rate_tensors(gradu, Ny):
 
     return sij, oij
 
-def compute_dissipation(sij, nu, Ny):
-
-    eps = np.empty([Ny])
-    for ii in range(Ny):
-        eps[ii] = -2 * nu * np.tensordot(sij[ii,:,:], sij[ii,:,:])
-
-    return eps
 
 def normalize_rate_tensors(sij, oij, tke, eps, Ny):
 
@@ -114,10 +102,11 @@ def compute_gradu(dUdy, Ny):
     gradu = np.empty([Ny, 3, 3])
     for ii in range(Ny):
         buf = np.zeros([3,3])
-        buf[0,1] = dUdy[ii]#meanData[ii,3]
+        buf[0,1] = dUdy[ii]
         gradu[ii, :, :] = buf
 
     return gradu
+
 
 def compute_cell_volumes(y, Ny):
 
@@ -138,14 +127,6 @@ def compute_cell_volumes(y, Ny):
     cellVols[-1] = dy * dx*dz
 
     return cellVols
-
-def compute_tke(flucData, Ny):
-
-    tke = np.empty([Ny,1])
-    for ii in range(Ny):
-        tke[ii,0] = flucData[ii,8]
-
-    return tke
 
 
 def compute_nut(aij, sij, Ny):
@@ -176,18 +157,14 @@ def compute_bij(uus, tke, Ny):
 
 
 
-
 def compute_qoi(shat, rhat, Ny):
 
     '''
     computes direct inputs to tbnn model
-    inputs: ?
     outputs: lam - scalar invariants
              basis - tensor basis
     '''
-
-    #shat, rhat = normalize_rate_tensors(sij, oij, tke, eps, Ny)
-
+ 
     basis = np.empty([Ny, 10, 3, 3])
     lam = np.empty([Ny,3])
     for i in range(Ny):
@@ -197,8 +174,5 @@ def compute_qoi(shat, rhat, Ny):
 
  
     return lam, basis
-
-
-
 
 
