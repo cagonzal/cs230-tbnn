f67a73eb107bfcdd39dd02b400e19a8aba9b5d20
Kim Liu <kimliu@stanford.edu>
2021-06-01 20:07:46 -0700

    Added shs data and loading

--- load_data.py
+++ load_data.py
@@ -1,5 +1,33 @@
 import numpy as np 
 
+def load_shs_data(filepath):
+    data = np.load(filepath)
+
+    # mean data
+    y = data['y']
+    U = data['u1'] 
+    dUdy = data['du1']
+
+    # fluc data
+    uus = np.zeros([Ny, 6])
+    uus[:,1] = data['uu']
+    uus[:,2] = data['vv']
+    uus[:,3] = data['ww']
+    uus[:,4] = data['uv']
+    uus[:,5] = data['uw']
+    uus[:,6] = data['vw']
+
+    tke = data['tke']
+
+    # tke data
+    eps = data['eps']
+
+    Ny = y.shape[0]
+
+    return Ny, y, U, dUdy, uus, tke, eps
+
+
+
 def load_fluc_data(filepath,Ny):
     ncols = 9
     count = 0
